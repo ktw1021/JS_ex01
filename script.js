f669ab343e115d4b90061aa011ecb9f5222c43c3
taewon Kim <ktw1398@gmail.com>
2024-06-03 16:54:20 +0900

    06-03

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
-        $(document).ready(event => {
-            $("#btnCallMovies").on("click", event => {
-                $.ajax({
-                    url: "http://localhost:3000/movies",
-                    type: "GET",
-                    dataType: "json",
-                    success: function(response) {
-                        console.log(response);
-                        $("#list").html(response);  //  JSON
-
-                        // 영화 목록 정보
-                        const movies = response.movies;
-                        console.log(movies); 
-                        movies.forEach(movie => {
-                            const row = `<tr>
-                                <td>${movie.id}</td>
-                                <td><a href="https://namu.wiki/w/${movie.title}" target="_blank">${movie.title}</a></td>
-                                <td>${movie.year}</td>
-                                <td><img src = "http://localhost:3000/images/${movie.image}" alt = "&${movie.title} 이미지" width="300"</td>
-                                <td>${movie.director}</td>
-                                </tr>`;
-                                $("#list").append(row);
-                        })
-                    },
-                    error: function(request, status, error) {
-                        console.error(error);
-                    }
+$(document).ready(event => {
+    $("#btnCallMovies").on("click", event => {
+        fetch("http://localhost:3000/movies")
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                const movies = data.movies;
+                movies.forEach(movie => {
+                    const row = `<tr>
+                    <td>${movie.id}</td>
+                    <td><a href="https://namu.wiki/w/${movie.title}" target="_blank">${movie.title}</a></td>
+                    <td>${movie.year}</td>
+                    <td><img src = "http://localhost:3000/images/${movie.image}" alt = "&${movie.title} 이미지" width="300"</td>
+                    <td>${movie.director}</td>
+                    </tr>`;
+                    $("#list").append(row);
                 })
             })
-        });
+            .catch(error => {
+                console.error(error);
+            })
+    })
+})
